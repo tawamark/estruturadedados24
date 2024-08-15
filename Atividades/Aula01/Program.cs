9644515c9f8cfca3af3bf0987ea202dce12e74e4
Thauan Gustavo Kerber <256166@unoesc.lan>
2024-08-14 22:07:22 -0300

    Primeiro projeto .net

---
+++ Atividades/Aula01/Program.cs
@@ -0,0 +1,17 @@
+﻿// comentário de uma linha
+
+/*
+    comentário de 
+    múltiplas linhas
+*/
+
+int number; // Declarando variável
+number = 10;//Atribuindo valor
+
+const int DAYS_IN_WEEK = 7; // Declarando constante
+
+Console.WriteLine( $"A semana tem {DAYS_IN_WEEK}, dias"); //imprimindo String concatenada
+
+
+
+
