d139e476762e1522e3b7a115ddfa5ccf196fae33
tawamark <thauangustavok@gmail.com>
2024-08-28 22:31:21 -0300

    normal

---
+++ Atividades/Aula02/Person.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula02
+{
+    public class Person
+    {
+        public string _location = string.Empty;
+        public string  Name {get; set;}
+        public int Age {get; set;}
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        
+        public Person ()
+        {
+            Name = "-------";
+        }
+
+        //Método para realocar a pessoa
+        public void Realocate(string location)
+        {
+            if(!string.IsNullOrEmpty(location))
+            {
+                _location = location;
+            }
+        }
+
+        public float GetDistance( string location)
+        {
+            return 0;
+        }
+    }
+
+}
---
+++ Exercicio02/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] pares = new int[50];
+int[] impares = new int[50];
+
+int contpar = 0;
+int contimpar = 0;
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 2 == 0)
+    {
+        pares[contpar] = i;
+        contpar++;
+    }
+    else
+    {
+        impares[contimpar] = i;
+        contimpar++;
+    }
+}
+
+foreach (int np in pares)
+{
+    Console.WriteLine(np);
+}
+
+Console.WriteLine("----------------------------");
+
+foreach (int ni in impares)
+{
+    Console.WriteLine(ni);
+}
