b465fae470527b6d728623dd523de59ba93bf2a9
Thauan Gustavo Kerber <256166@unoesc.lan>
2024-09-11 22:01:53 -0300

    Exercicio05

---
+++ Atividades/Pilhas/Program.cs
@@ -0,0 +1,25 @@
+﻿// Basicamente as pilas trabalham com uma etrututra LIFO -> Last in First OUT
+// Três métodos importantes:
+// Push() -> insere um elemento no topo da pilha
+// Pop() -> remove um elemento do topo e o retorna
+// Peek() -> retorna o elemento ao topo sem remove-lo
+
+//Invertendo as bolas
+Stack<char> caracteres = new Stack<char> ();
+foreach(char c in "DARCY DA MONATANHA")
+{
+    caracteres.Push (c);
+}
+
+//Desempihando
+string invertido = string.Empty;
+while(caracteres.Count > 0)
+{
+    invertido += caracteres.Pop ();
+}
+Console.WriteLine (invertido);
+
+// faça em programa que leia a entrada do usuário e identifique se a palavra informada é ou não um palindromo
+
+
+
---
+++ Exercicios/Exercicio05/Program.cs
@@ -0,0 +1,31 @@
+﻿Stack<char> caracteres = new Stack<char>();
+
+Console.WriteLine ("Digite uma palavra:");
+string? palavra = Console.ReadLine ();
+
+foreach(char c in palavra)
+{
+    caracteres.Push (c);
+}
+
+//Desempihando
+string invertido = string.Empty;
+while(caracteres.Count > 0)
+{
+    invertido += caracteres.Pop ();
+}
+Console.WriteLine (invertido);
+
+Console.WriteLine ("as palavras são iguais?");
+Console.WriteLine ("1 - sim | 2 - não");
+string? resposta = Console.ReadLine ();
+
+switch (resposta)
+{
+    case "1":
+    Console.WriteLine ("a palavra é um palindro");
+    break;
+    case  "2":
+    Console.WriteLine ("a palavra não é um palindro");
+    break;
+}
